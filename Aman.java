08ac8d7ed5f114cb2af129588c6ab022e0b4dc57
Aman Kannaujiya <98861578+amankannaujiya799@users.noreply.github.com>
2025-05-19 21:25:58 +0530

    Add files via upload

---
+++ Aman.java
@@ -0,0 +1,11 @@
+public class Aman {
+    public static void main (String args [])
+    {
+        TreeMap<String,Integer>tm  = new TreeMap<();
+        tm.put("aman", 500);
+        tm.put("raj"  , 8100);
+        System.out.println(tm);
+
+
+}
+}
