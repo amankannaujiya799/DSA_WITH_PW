2ff553f9d78ddf40ddeba84e9cbe020fe38223c6
Aman Kannaujiya <98861578+amankannaujiya799@users.noreply.github.com>
2025-06-28 17:31:32 +0530

    Add files via upload

---
+++ PrintNumbers.java
@@ -0,0 +1,7 @@
+public class PrintNumbers {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println("Hello Aman"); // Prints each number on a new line
+        }
+    }
+}
