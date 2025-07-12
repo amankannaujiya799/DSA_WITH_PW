51ebfa8afdd54b0ff53354706a11738ae02912dd
Aman Kannaujiya <98861578+amankannaujiya799@users.noreply.github.com>
2025-07-12 15:54:01 +0530

    Add files via upload

--- Array.java
+++ Array.java
@@ -28,3 +28,10 @@ public class Array {
 
 
 }
+public class PrintNumbers {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i); // Prints each number on a new line
+        }
+    }
+}
