590e1a5ec280541211bb2fcc33f122418e4d2948
Aman Kannaujiya <98861578+amankannaujiya799@users.noreply.github.com>
2025-07-27 22:53:58 +0530

    Add files via upload

---
+++ Construtor.java
@@ -0,0 +1,17 @@
+class Construtor {
+    public static void main(String[] args) {
+        Student s1 = new Student("Aman");
+        System.out.println(s1.name);
+
+        
+    }
+}
+class Student{
+    String name; 
+    int roll;
+    Sutudent(String name){  
+        this.name = name;
+       // System.out.println();//
+
+    }
+}
